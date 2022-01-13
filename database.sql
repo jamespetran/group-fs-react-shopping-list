a369b91faff1f7380534fa4e6f45664c58b3549d
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2022-01-13 10:56:17 -0600

    Wrote router GET request

--- database.sql
+++ database.sql
@@ -1,12 +1,14 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+--create database 'fs-react-shopping'
+
 CREATE TABLE groceries (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(80) NOT NULL,
-    "quantity" DECIMAL,
+    "quantity" DECIMAL NOT NULL,
     "unit" VARCHAR(20),
     "purchased" BOOLEAN DEFAULT FALSE
 );
 
 INSERT INTO groceries ("name", "quantity", "unit")
-VALUES ('banannas', 3, 'bundles'), ('eggs', 4, 'dozen')
+VALUES ('banannas', 3, 'bundles'), ('eggs', 4, 'dozen');
--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,19 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
-
+router.get('/', (req, res) => {
+    console.log('In router GET');
+    const queryText = `
+                        SELECT * FROM "groceries"
+                        `;
+    pool.query(queryText)
+    .then((result) => {
+        res.send(result.rows);
+    })
+    .catch((err) => {
+        console.log('Error: Could not retrieve list', err);
+        res.sendStatus(500);
+    });
+});// end GET
 
 module.exports = router;
