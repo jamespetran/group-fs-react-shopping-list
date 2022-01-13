eb07520a5527480488597e9fc81eb590f0716815
Toivo Hannigan <THannigan046@gmail.com>
2022-01-13 10:50:08 -0600

    made a router.post I think

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,22 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
-
+router.post('/', (req, res) => {
+    const item = req.body
+    const sqlText = `INSERT INTO groceries(name, quantity, unit)
+                      VALUES ($1, $2, $3)`
+    
+    pool.query(sqlText, [item.name, item.quantity, item.unit])
+        .then((result) => {
+            console.log('added item to the database', result);
+            res.sendStatus(201);
+            
+        })
+        .catch((error) => {
+            console.log(`error making database query ${sqlText}`, error);
+            res.sendStatus(500);
+            
+        })
+})
 
 module.exports = router;
