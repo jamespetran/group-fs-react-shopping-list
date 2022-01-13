82a36b859ebb9685e983bdcbaad5e9c209addf22
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2022-01-13 10:35:10 -0600

    Wrote sql db setup

--- database.sql
+++ database.sql
@@ -1,2 +1,12 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+CREATE TABLE groceries (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(80) NOT NULL,
+    "quantity" DECIMAL,
+    "unit" VARCHAR(20),
+    "purchased" BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO groceries ("name", "quantity", "unit")
+VALUES ('banannas', 3, 'bundles'), ('eggs', 4, 'dozen')
