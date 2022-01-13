4e4a136164536b53e639f968f80e166cca131b21
Jacob Larson <jacob.larson55112@gmail.com>
2022-01-13 11:52:22 -0600

    added axios GET

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 import Header from '../Header/Header'
 import Form from '../Form/Form'
 import './App.css';
 
-
 function App() {
+    const [groceryList, setGroceryList] = useState([]);
+    useEffect(() => {
+        fetchList();
+    }, []);
+
+    const fetchList = () => {
+    axios.get('/grocery-list').then((res) => {
+        console.log('GET List', res);
+        setGroceryList(res.data)
+    }).catch((err) => {
+        console.log('ERR GET list', err);
+    })
+    }
+
     return (
         <div className="App">
             <Header />
